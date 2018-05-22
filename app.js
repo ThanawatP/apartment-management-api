2056baf4957ca4531d53ec070b7d05380654987a
ThanawatP <taeint93@gmail.com>
2018-05-22 12:02:26 +0700

    change cors link

--- app.js
+++ app.js
@@ -17,9 +17,13 @@ const UPLOAD_PATH = require('variables').UPLOAD_PATH;
 const cleanFolder = require('utils').cleanFolder;
 cleanFolder(UPLOAD_PATH);
 
-const corsPort = process.env.CORS_PORT || 4200;
+const corsPort = process.env.CORS_PORT || "4200";
+let corsLink = `http://localhost:${corsPort}`
+if (corsPort == "80") {
+  corsLink = "http://localhost"
+}
 const corsOptions = {
-  origin: `http://localhost:${corsPort}`,
+  origin: corsLink,
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 };
 
